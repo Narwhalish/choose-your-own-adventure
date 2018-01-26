841e293e90bfcd93a0af6e39d1603bf7dd187339
emiliu2019 <emiliu@ctemc.org>
2018-01-26 09:19:27 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Name: Emily Liu, Bryan Yao, Karena Yan 
+Course: CSE 
+Assignment: Choose your Own Adventure
+Description: Billy Pilgrim's Adventure Through Time
+"""
+
+def main():
+    
+if __name__ == '___main___':
+    main()

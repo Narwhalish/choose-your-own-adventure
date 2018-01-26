7b5143cd1da46068e5676b771a0f56c827e0ef4a
Narwhalish <bryao@ctemc.org>
2018-01-26 10:01:32 -0500

    Add room.py generation and main_menu.py gui

---
+++ main_menu.py
@@ -0,0 +1,6 @@
+from Tkinter import *
+
+class MainMenu(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        
---
+++ room.py
@@ -0,0 +1,5 @@
+from Tkinter import *
+
+class Room(Frame):
+    def __init__(self, parent, dimensions, objects):
+        Frame.__init__(self,

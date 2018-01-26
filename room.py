ce08696781e4fb10972a371ca796b77aa5692a2f
Narwhalish <bryao@ctemc.org>
2018-01-26 16:43:16 -0500

    Changes to room.py

--- room.py
+++ room.py
@@ -2,4 +2,6 @@ from Tkinter import *
 
 class Room(Frame):
     def __init__(self, parent, dimensions, objects):
-        Frame.__init__(self,
+        Frame.__init__(self, parent)
+        self.parent = parent
+        

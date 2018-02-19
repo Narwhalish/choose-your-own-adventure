04565d571058c1f6a852296aa00d053d1130f5dc
emiliu2019 <emiliu@ctemc.org>
2018-02-19 01:03:05 -0500

    Added items
    Allowed users to find items
    Made items() and various item functions

--- slaughterhouse.py
+++ slaughterhouse.py
@@ -10,16 +10,31 @@ In the slaughterhouse there are:
     Spoon
     Other Americans
     Shrapnel
+    
+** User Notes: Type %matplotlib inline in the kernel before playing game **
 ================================================================================
 """
 import matplotlib.pyplot as plt
 
 global space, position
 space = [[0]*5 for i in range(5)]
-global prow, pcolumn
-prow = 2
+
+space[4][2]="Entrance"
+doorpos = [0,3]
+guardpos = [0, 1]
+syruppos = [1, 4]
+spoonpos = [4, 0]
+amerpos = [3, 3]
+shrapnelpos = [2, 1]
+
+global prow, pcolumn, position
+prow = 4
 pcolumn = 2
 x = "You"
+position = [prow, pcolumn]
+
+global door
+found_door = False
 
 def print_space():
     global space
@@ -28,33 +43,42 @@ def print_space():
         print '\n'
 
 def make_map():
-    global space
-    print "global"
-    print_space()
+    global space, position
+    #print_space()
     print "\n MAP OF SLAUGHTERHOUSE"
     color_grid = [['black']*5 for i in range(5)]
     
     pmap = plt.table(cellText=space, cellColours=color_grid,
             cellLoc='center', rowLoc='left', colLoc = 'center',
             loc='center', bbox=None)
+
     pmap.scale(1, 3.65)
     plt.xticks([])
     plt.yticks([])
     table_cells = pmap.properties()['child_artists']
+    
     for cell in table_cells:
         cell._text.set_color('white')
         cell.set_edgecolor('white')
+    
     plt.show()
 
 def action():
-    global prow, pcolumn, space
+    global prow, pcolumn, space, position
     a = raw_input('Up, Down, Right, Left (U,D,R,L): ')
     if ( a == 'U'):
         space[prow][pcolumn]=0
         if prow!=0:
             prow=prow-1
-            space[prow][pcolumn]=x
-            make_map()
+            position = [prow, pcolumn]
+            #print "Position = " + str(position)
+            space[position[0]][position[1]]=x
+            if (position==doorpos or position==guardpos or  
+                position==syruppos or position==spoonpos or
+                position==amerpos or position==shrapnelpos):
+                items()
+            else:
+                make_map()
         else:
             print "You can't walk through a wall!"
             action() 
@@ -62,8 +86,15 @@ def action():
         space[prow][pcolumn]=0
         if prow!=4:
             prow=prow+1
-            space[prow][pcolumn]=x
-            make_map()
+            position = [prow, pcolumn]
+            #print "Position = " + str(position)
+            space[position[0]][position[1]]=x
+            if (position==doorpos or position==guardpos or  
+                position==syruppos or position==spoonpos or
+                position==amerpos or position==shrapnelpos):
+                items()
+            else:
+                make_map()
         else:
             print "You can't walk through a wall!"
             action() 
@@ -71,8 +102,15 @@ def action():
         space[prow][pcolumn]=0
         if pcolumn!=0:
             pcolumn=pcolumn-1
-            space[prow][pcolumn]=x
-            make_map()
+            position = [prow, pcolumn]
+            #print "Position = " + str(position)
+            space[position[0]][position[1]]=x
+            if (position==doorpos or position==guardpos or  
+                position==syruppos or position==spoonpos or
+                position==amerpos or position==shrapnelpos):
+                items()
+            else:
+                make_map()
         else:
             print "You can't walk through a wall!"
             action() 
@@ -80,17 +118,65 @@ def action():
         space[prow][pcolumn]=0
         if pcolumn!=4:
             pcolumn=pcolumn+1
-            space[prow][pcolumn]=x
-            make_map()
+            position = [prow, pcolumn]
+            #print "Position = " + str(position)
+            space[position[0]][position[1]]=x
+            if (position==doorpos or position==guardpos or  
+                position==syruppos or position==spoonpos or
+                position==amerpos or position==shrapnelpos):
+                items()
+            else:
+                make_map()
         else:
             print "You can't walk through a wall!"
             action() 
 
+def door():
+    print "You found a door!"
+def guards():
+    print "You found three guards!"
+def syrup():
+    print "You found a bottle of syrup!"
+def spoon():
+    print "You found a spoon!"
+def americans():
+    print "You found your fellow Americans!"
+def shrapnel():
+    print "You found shrapnel!"
+    
+def items():
+    decisions = {str(doorpos): door,
+                str(guardpos): guards,
+                str(syruppos): syrup,
+                str(spoonpos): spoon,
+                str(amerpos): americans,
+                str(shrapnelpos): shrapnel}
+    decisions[str(position)]()
+
+    #decision = None
+    # if (position==doorpos):
+    #     door()
+    # elif (position==guardpos):
+    #     guards()
+    # elif (position==syruppos):
+    #     syrup()
+    # elif (position==spoonpos):
+    #     spoon()
+    # elif (position==amerpos):
+    #     americans()
+    # elif (position==shrapnelpos):
+    #     shrapnel()
+    
 def main():
-    global space
+    print "\n You have discovered the Slaughterhouse!"
+    
+    global space, door, position
     space[prow][pcolumn]=x
     make_map()
-    action()
+    
+    while (found_door==False):
+        action()
+    
     
 if __name__ == '__main__':
     main()

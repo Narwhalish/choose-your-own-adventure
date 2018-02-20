8e7d5647913bcb7a75ad8ff2ec9b248c429f7858
emiliu2019 <emiliu@ctemc.org>
2018-02-19 22:12:06 -0500

    Added many item functions
    Added item functions, if else statements to enter these functions

--- slaughterhouse.py
+++ slaughterhouse.py
@@ -6,12 +6,14 @@ Billy Pilgrim's Adventure Through Time
 Slaughterhouse Room (Need Certain Number of Items)
 In the slaughterhouse there are:
     Guards
-    Syrup
-    Spoon
+    Syrup - Boosts HP
+    Spoon - Must have to eat syrup
     Other Americans
     Shrapnel
+    Shovel - Key to leave the room 
     
-** User Notes: Type %matplotlib inline in the kernel before playing game **
+** User Notes: Type %matplotlib inline in the kernel before playing game
+                All user entries are case-insensitive**
 ================================================================================
 """
 import matplotlib.pyplot as plt
@@ -20,17 +22,23 @@ global space, position
 space = [[0]*5 for i in range(5)]
 
 space[4][2]="Entrance"
+space[0][1]="Guards"
+space[0][3]="Door"
+space[3][3]="Americans"
+space[1][4]="Syrup"
+entrancepos=[4,2]
 doorpos = [0,3]
 guardpos = [0, 1]
 syruppos = [1, 4]
 spoonpos = [4, 0]
 amerpos = [3, 3]
 shrapnelpos = [2, 1]
+shovelpos = [2,2]
 
 global prow, pcolumn, position
 prow = 4
 pcolumn = 2
-x = "You"
+x = "YOU"
 position = [prow, pcolumn]
 
 global door
@@ -65,7 +73,7 @@ def make_map():
 
 def action():
     global prow, pcolumn, space, position
-    a = raw_input('Up, Down, Right, Left (U,D,R,L): ')
+    a = raw_input('Up, Down, Right, Left (U,D,R,L): ').upper()
     if ( a == 'U'):
         space[prow][pcolumn]=0
         if prow!=0:
@@ -75,7 +83,8 @@ def action():
             space[position[0]][position[1]]=x
             if (position==doorpos or position==guardpos or  
                 position==syruppos or position==spoonpos or
-                position==amerpos or position==shrapnelpos):
+                position==amerpos or position==shrapnelpos or 
+                position==shovelpos or position==entrancepos):
                 items()
             else:
                 make_map()
@@ -91,7 +100,8 @@ def action():
             space[position[0]][position[1]]=x
             if (position==doorpos or position==guardpos or  
                 position==syruppos or position==spoonpos or
-                position==amerpos or position==shrapnelpos):
+                position==amerpos or position==shrapnelpos or 
+                position==shovelpos or position==entrancepos):
                 items()
             else:
                 make_map()
@@ -107,7 +117,8 @@ def action():
             space[position[0]][position[1]]=x
             if (position==doorpos or position==guardpos or  
                 position==syruppos or position==spoonpos or
-                position==amerpos or position==shrapnelpos):
+                position==amerpos or position==shrapnelpos or
+                position==shovelpos or position==entrancepos):
                 items()
             else:
                 make_map()
@@ -123,7 +134,8 @@ def action():
             space[position[0]][position[1]]=x
             if (position==doorpos or position==guardpos or  
                 position==syruppos or position==spoonpos or
-                position==amerpos or position==shrapnelpos):
+                position==amerpos or position==shrapnelpos or 
+                position==shovelpos or position==entrancepos):
                 items()
             else:
                 make_map()
@@ -131,18 +143,66 @@ def action():
             print "You can't walk through a wall!"
             action() 
 
+def entrance():
+    space[doorpos[0]][doorpos[1]]="YOU\nDoor"
+    make_map()
+    print "Do you want to leave the slaughterhouse?"
+    d = (raw_input("To leave, press L. To stay, press S: ")).upper()
+    if (d =='L'):
+        pass #leads to last room 
+    elif (d == 'S'):
+        print "You decided to stay in the slaughterhouse."
+        print "Maybe there are more items!"
+        action()
+
 def door():
+    space[doorpos[0]][doorpos[1]]="YOU\nDoor"
+    make_map()
     print "You found a door!"
+    print "I wonder where this door leads... Will you open the door?"
+    d = (raw_input("To enter, press E. To stay, press S: ")).upper()
+    if (d =='E'):
+        pass #leads to new room 
+    elif (d == 'S'):
+        print "You decided to stay in the slaughterhouse."
+        print "Maybe there are more items!"
+        action()
 def guards():
+    space[doorpos[0]][doorpos[1]]="YOU\nGuards"
+    make_map()
     print "You found three guards!"
+    print "Maybe they have information. Will you talk to them?"
+    d = (raw_input("To talk, press T. To ignore, press I: ")).upper()
+    if (d =='T'):
+        print "One of the guards sees and turns to you."
+        print "'Stay inside! Dresden is being bombed.'"
+        a = (raw_input("a. 'How can I leave?'\n \
+                        b. 'What's the syrup for?' \n \
+                        c. '
+    elif (d == 'I'):
+        print "You decided to ignore the guards."
+        action()
+    
 def syrup():
     print "You found a bottle of syrup!"
+    space[syruppos[0]][syruppos[1]]="Syrup"
+    make_map()
 def spoon():
     print "You found a spoon!"
+    space[spoonpos[0]][spoonpos[1]]="Spoon"
+    make_map()
 def americans():
     print "You found your fellow Americans!"
+    space[amerpos[0]][amerpos[1]]="Americans"
+    make_map()
 def shrapnel():
     print "You found shrapnel!"
+    space[shrapnelpos[0]][shrapnelpos[1]]="Shrapnel"
+    make_map()
+def shovel():
+    print "You found a shovel!"
+    space[shovelpos[0]][shovelpos[1]]="Shovel"
+    make_map()
     
 def items():
     decisions = {str(doorpos): door,
@@ -150,22 +210,10 @@ def items():
                 str(syruppos): syrup,
                 str(spoonpos): spoon,
                 str(amerpos): americans,
-                str(shrapnelpos): shrapnel}
+                str(shrapnelpos): shrapnel
+                str(shovelpos): shovel,
+                str(entrancepos): entrancepos}
     decisions[str(position)]()
-
-    #decision = None
-    # if (position==doorpos):
-    #     door()
-    # elif (position==guardpos):
-    #     guards()
-    # elif (position==syruppos):
-    #     syrup()
-    # elif (position==spoonpos):
-    #     spoon()
-    # elif (position==amerpos):
-    #     americans()
-    # elif (position==shrapnelpos):
-    #     shrapnel()
     
 def main():
     print "\n You have discovered the Slaughterhouse!"

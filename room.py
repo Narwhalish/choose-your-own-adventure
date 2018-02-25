499b72c648e1c0332b4185b423101f163f004461
Narwhalish <bryao@ctemc.org>
2018-02-25 16:44:16 -0500

    Add dialogue window

--- room.py
+++
@@ -1,7 +0,0 @@
-from Tkinter import *
-
-class Room(Frame):
-    def __init__(self, parent, dimensions, objects):
-        Frame.__init__(self, parent)
-        self.parent = parent
-        
--- title_screen.py
+++ title_screen.py
@@ -1,8 +1,8 @@
 from Tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
-import tkFont
-import webbrowser
+import tkFont, webbrowser, os
+import main
 """
 Name: Emily Liu, Bryan Yao, Karena Yan 
 Course: CSE 
@@ -12,45 +12,55 @@ Description: Billy Pilgrim -- Lost in Time
 Creates interactive title screen with Tk elements and PIL objects.
 """
 
-
-def OpenURL(url):
 """Opens a given URL with using the webbrowser module
 Args:
     URL (str)
 Returns:
     N/A
 """
+def OpenURL(url):
     webbrowser.open_new(url)
+    
+def SwitchFrames(new, old):
+    new.grid(row=0, column=0, sticky=N+E+S+W)
+    old.grid_remove()
 
 """Initialize main root window of title screen."""
 root = Tk()
 root.title('Adventure in Time')
+
+"""Create screen frames"""
 mainframe = Frame(root, bg='DarkGoldenRod4')
 mainframe.grid(column=0, row=0, sticky=N+E+S+W)
+dialogueframe = Frame(root)
 
 """Read image data from stored background image and compound it with a label."""
-imgBG = Image.open('images/welcome_display.jpg')
-welcome_display = ImageTk.PhotoImage(imgBG)
+mainframe_imgBG = Image.open('images/welcome_display.jpg')
+welcome_display = ImageTk.PhotoImage(mainframe_imgBG)
 welcome_font = tkFont.Font(family='Helvetica', size=42, weight='bold', underline=1)
 welcome_screen = Label(mainframe, image=welcome_display, text='Adventure in Time', compound=CENTER, font=welcome_font, fg='white')
 welcome_screen.grid(column=0, row=0, columnspan=3, rowspan=2, sticky=N+E+S+W)
 
 """Create Play button with callback to main.py"""
-btnPlay = ttk.Button(mainframe, text='Play', command=pass)
-btnPlay.grid(column=0, row=1, columnspan=3, sticky=E+W, padx=30)
+mainframe_btnPlay = ttk.Button(mainframe, text='Play', command=lambda: SwitchFrames(dialogueframe, mainframe))
+mainframe_btnPlay.grid(column=0, row=1, columnspan=3, sticky=E+W, padx=30)
 
 """Create About button with callback to README.md"""
-btnAbout = ttk.Button(mainframe, text='About')
-btnAbout.grid(column=0, row=2, padx=5, pady=5)
+mainframe_btnAbout = ttk.Button(mainframe, text='About')
+mainframe_btnAbout.grid(column=0, row=2, padx=5, pady=5)
 
 """Create Source button with callback to OpenURL to open source code on GitHub"""
-url="https://github.com/Narwhalish/choose-your-own-adventure"
-btnSource = ttk.Button(mainframe, text='Source', command=lambda: OpenURL(url))
-btnSource.grid(column=1, row=2, padx=5, pady=5)
+source_url="https://github.com/Narwhalish/choose-your-own-adventure"
+mainframe_btnSource = ttk.Button(mainframe, text='Source', command=lambda: OpenURL(source_url))
+mainframe_btnSource.grid(column=1, row=2, padx=5, pady=5)
 
 """Create Quit button to destroy root window and widgets."""
-btnQuit = ttk.Button(mainframe, text='Quit', command=lambda: root.destroy())
-btnQuit.grid(column=2, row=2, padx=5, pady=5)
+mainframe_btnQuit = ttk.Button(mainframe, text='Quit', command=lambda: root.destroy())
+mainframe_btnQuit.grid(column=2, row=2, padx=5, pady=5)
+
+dialogueframe_lblNarrativeVar = StringVar()
+dialogueframe_lblNarrative = Label(dialogueframe, relief=RIDGE, textvariable=dialogueframe_lblNarrativeVar)
+dialogueframe_lblNarrative.grid(column=0, row=0, padx=5, pady=5)
 
 """Start mainloop"""
 root.mainloop()

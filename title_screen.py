533fdbbb9eb3bf5d825b04c6864c7de687bcb038
Narwhalish <bryao@ctemc.org>
2018-02-25 13:26:22 -0500

    Fix image display

--- title_screen.py
+++ title_screen.py
@@ -1,16 +1,20 @@
 from Tkinter import *
 from tkinter import ttk
+from PIL import Image, ImageTk
+import tkFont
 
 root = Tk()
 root.title("Adventure in Time")
 mainframe = Frame(root, padx=3, pady=12)
-mainframe.grid(column=0, row=0, sticky=(N, E, S, W))
+mainframe.grid(column=0, row=0)
 mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)	
 
-welcome_display = BitmapImage('welcome_screen.XBM')
+welcome_display_img = Image.open('welcome_display.jpg')
+welcome_display = ImageTk.PhotoImage(welcome_display_img.resize((welcome_display_img.size[0]/3, welcome_display_img.size[1]/3), Image.BILINEAR))
 welcome_title = "Adventure in Time"
-welcome_screen = Label(mainframe, image=welcome_display, text=welcome_title)
-welcome_screen.grid()
+welcome_font = Font(family=
+welcome_screen = Label(mainframe, image=welcome_display, text=welcome_title, compound=CENTER)
+welcome_screen.grid(column=1, row=1)
 
 root.mainloop()
